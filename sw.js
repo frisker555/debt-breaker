36f2a1c8272114ba65a6b65233fcb5106237d85c
frisker555 <hi.speed.kobashiri.0802@gmail.com>
2026-09-05 22:21:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION="18.67";
+const APP_VERSION="18.74";
 const CACHE_NAME=`debt-breaker-v${APP_VERSION}-shell-v1`;
 const BASE_URL=new URL("./",self.location).href;
 const INDEX_URL=new URL("index.html",BASE_URL).href;
